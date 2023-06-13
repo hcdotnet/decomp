b8603916c898f503498f24c8e02d47b8d326b4cc
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 16:12:18 -0700

    Global init scripts dumper.

--- assets/AssetDumper/AssetDumper/Dumpers/GlobalInitDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/GlobalInitDumper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 using UndertaleModLib;
 using UndertaleModLib.Models;
 
@@ -6,9 +8,13 @@ namespace AssetDumper.Dumpers;
 
 [Dumper("global_init_scripts")]
 public sealed class GlobalInitDumper : AbstractListDumper<UndertaleGlobalInit> {
-    protected override void DumpListItem(UndertaleData data, UndertaleGlobalInit item, FileWriter w) {
-        // TODO
+    public override void Dump(UndertaleData data, FileWriter w) {
+        // base.Dump(data, w);
+
+        w.Create(JsonConvert.SerializeObject(data.GlobalInitScripts.Select(x => x.Code.Name.Content), Formatting.Indented), "global_init_scripts.json");
     }
+
+    protected override void DumpListItem(UndertaleData data, UndertaleGlobalInit item, FileWriter w) { }
 
     protected override IList<UndertaleGlobalInit>? GetList(UndertaleData data) {
         return data.GlobalInitScripts;
