608afe71e565b2343a8a16eb8ecf8ff509880c9a
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 16:14:38 -0700

    Dump audio groups.

--- assets/AssetDumper/AssetDumper/Dumpers/AudioGroupDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/AudioGroupDumper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 using UndertaleModLib;
 using UndertaleModLib.Models;
 
@@ -6,9 +8,13 @@ namespace AssetDumper.Dumpers;
 
 [Dumper("audio_groups")]
 public sealed class AudioGroupDumper : AbstractListDumper<UndertaleAudioGroup> {
-    protected override void DumpListItem(UndertaleData data, UndertaleAudioGroup item, FileWriter w) {
-        // TODO
+    public override void Dump(UndertaleData data, FileWriter w) {
+        // base.Dump(data, w);
+        
+        w.Create(JsonConvert.SerializeObject(data.AudioGroups.Select(x => x.Name.Content), Formatting.Indented), "audio_groups.json");
     }
+
+    protected override void DumpListItem(UndertaleData data, UndertaleAudioGroup item, FileWriter w) { }
 
     protected override IList<UndertaleAudioGroup>? GetList(UndertaleData data) {
         return data.AudioGroups;
