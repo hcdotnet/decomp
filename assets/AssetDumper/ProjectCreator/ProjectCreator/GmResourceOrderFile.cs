f29c19aafa51f32c8d77f79d8a9c49c99cb0402c
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 21:20:34 -0700

    Define GmResourceOrderFile and its dependent types.

---
+++ assets/AssetDumper/ProjectCreator/ProjectCreator/GmResourceOrderFile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using ProjectCreator.ProjectCreator.Resources;
+
+namespace ProjectCreator.ProjectCreator; 
+
+public sealed class GmResourceOrderFile {
+    [JsonProperty("ResourceOrderSettings")]
+    public List<NameAndPathOrder> ResourceOrderSettings { get; set; }
+
+    [JsonProperty("FolderOrderSettings")]
+    public List<NameAndPathOrder> FolderOrderSettings { get; set; }
+}
---
+++ assets/AssetDumper/ProjectCreator/ProjectCreator/Resources/NameAndPathOrder.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace ProjectCreator.ProjectCreator.Resources;
+
+public sealed class NameAndPathOrder {
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("path")]
+    public string Path { get; set; }
+
+    [JsonProperty("order")]
+    public int Order { get; set; }
+}
