280e26ab1c16a49f2f0203a3558766c5ce06739e
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 20:49:01 -0700

    Okay I can't be asked to work on specific dumpers. It's better to just use the existing UMT infrastructure.

--- assets/AssetDumper/AssetDumper/Dumpers/RoomDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/RoomDumper.cs
@@ -4,6 +4,50 @@ using UndertaleModLib.Models;
 
 namespace AssetDumper.Dumpers;
 
+public class RoomInfo {
+    public string Name { get; init; }
+
+    public string Caption { get; init; }
+
+    public uint Width { get; init; }
+
+    public uint Height { get; init; }
+
+    public uint Speed { get; init; }
+
+    public bool Persistent { get; init; }
+
+    public uint BackgroundColor { get; init; }
+
+    public bool DrawBackgroundColor { get; init; }
+
+    public string CreationCodeIdName { get; init; }
+
+    // public RoomEntryFlagsInfo Flags { get; init; }
+
+    public bool World { get; init; }
+
+    public uint Top { get; init; }
+
+    public uint Left { get; init; }
+
+    public uint Right { get; init; }
+
+    public uint Bottom { get; init; }
+
+    public float GravityX { get; init; }
+
+    public float GravityY { get; init; }
+
+    public float MetersPerPixel { get; init; }
+
+    public double GridWidth { get; init; }
+
+    public double GridHeight { get; init; }
+
+    public double GridThicknessPx { get; init; }
+}
+
 [Dumper("rooms")]
 public sealed class RoomDumper : AbstractListDumper<UndertaleRoom> {
     protected override void DumpListItem(UndertaleData data, UndertaleRoom item, FileWriter w) {
