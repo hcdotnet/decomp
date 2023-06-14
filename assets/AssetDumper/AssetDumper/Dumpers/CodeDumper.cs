37e9f4fc01915da5106a8d4897d757588083b2d3
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 17:52:18 -0700

    Elaborated on TODOs for some dumpers.

--- assets/AssetDumper/AssetDumper/Dumpers/CodeDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/CodeDumper.cs
@@ -7,7 +7,7 @@ namespace AssetDumper.Dumpers;
 [Dumper("code")]
 public sealed class CodeDumper : AbstractListDumper<UndertaleCode> {
     protected override void DumpListItem(UndertaleData data, UndertaleCode item, FileWriter w) {
-        // TODO
+        // TODO: figure out how we want to dump code, and if we even want to
     }
 
     protected override IList<UndertaleCode>? GetList(UndertaleData data) {
--- assets/AssetDumper/AssetDumper/Dumpers/TexturePageItemDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/TexturePageItemDumper.cs
@@ -7,7 +7,7 @@ namespace AssetDumper.Dumpers;
 [Dumper("texture_page_items")]
 public sealed class TexturePageItemDumper : AbstractListDumper<UndertaleTexturePageItem> {
     protected override void DumpListItem(UndertaleData data, UndertaleTexturePageItem item, FileWriter w) {
-        // TODO
+        // TODO: eventually stop dumping sprites and make them point to here
     }
 
     protected override IList<UndertaleTexturePageItem>? GetList(UndertaleData data) {
