5abd02ce864e49e32049297e07ecc46032c8649f
Tomat <xxlennygamerxx@gmail.com>
2023-06-13 20:11:36 -0700

    Texture page item dumping.

--- assets/AssetDumper/AssetDumper/Dumpers/TexturePageItemDumper.cs
+++ assets/AssetDumper/AssetDumper/Dumpers/TexturePageItemDumper.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections.Generic;
 using UndertaleModLib;
 using UndertaleModLib.Models;
+using UndertaleModLib.Util;
 
 namespace AssetDumper.Dumpers;
 
-[Dumper("texture_page_items")]
+[Dumper("texture_page_items", Large = true)]
 public sealed class TexturePageItemDumper : AbstractListDumper<UndertaleTexturePageItem> {
     protected override void DumpListItem(UndertaleData data, UndertaleTexturePageItem item, FileWriter w) {
-        // TODO: eventually stop dumping sprites and make them point to here
+        // TODO: I believe this includes every texture in the game, including
+        // things covered by sprites and what-not. Can we just dump these and
+        // have others point to them?
+
+        var worker = new TextureWorker();
+        worker.ExportAsPNG(item, w.GetPath(item.Name.Content + ".png"));
     }
 
     protected override IList<UndertaleTexturePageItem>? GetList(UndertaleData data) {
